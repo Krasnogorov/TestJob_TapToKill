70c94e8b8d0aaa2ddbc46e4f95028093ab507721
Krasnogorov Sergey <shamanru@meta.ua>
2019-04-28 09:26:24 +0300

    Added script for main menu

---
+++ Tap To Kill V2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * PauseWindow.cs
+ * This code is part of demo project.
+ * Created by Sergey Krasnogorov.
+ * All rights reserved.
+ */
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Class for interfaces in main menu
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Callback for start button
+    /// </summary>
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene(Constants.MAIN_GAME_SCENE);
+    }
+}
